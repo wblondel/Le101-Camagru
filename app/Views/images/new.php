02ae6dd19de4adc4ec54556c9dd354a0ad946db3
William Blondel <contact@williamblondel.fr>
2018-06-14 10:38:20 +0200

    [Picture add]: uploading image works

--- app/Views/images/new.php
+++ app/Views/images/new.php
@@ -1,11 +1,21 @@
-<div class="contentarea">
-    <div class="camera">
-        <video id="video">Video stream not available.</video>
-        <button id="startbutton">Take photo</button>
+<main role="main">
+    <div class="py-5 bg-light">
+        <div class="container">
+            <div class="contentarea">
+                <div class="camera">
+                    <video id="video">Video stream not available.</video>
+                    <div class="text-center">
+                        <button id="startbutton">Take photo</button>
+                        <input type="file" id="selectedFile" style="display: none;" onchange="previewFile()" />
+                        <input id="uploadbutton" type="button" value="Upload photo" onclick="document.getElementById('selectedFile').click();" />
+                    </div>
+                </div>
+                <canvas id="canvas">
+                </canvas>
+                <div class="output">
+                    <img id="photo" alt="The screen capture will appear in this box.">
+                </div>
+            </div>
+        </div>
     </div>
-    <canvas id="canvas">
-    </canvas>
-    <div class="output">
-        <img id="photo" alt="The screen capture will appear in this box.">
-    </div>
-</div>
+</main>
--- public/js/camera.js
+++ public/js/camera.js
@@ -56,3 +56,21 @@ navigator.mediaDevices.getUserMedia({ audio: false, video: true })
     .catch(function(err) {
         console.log(err.name + ": " + err.message);
     });
+
+function previewFile() {
+    var preview = document.getElementById("photo");
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+    var imageType = /image.*/;
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file.type.match(imageType))
+    {
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+}
