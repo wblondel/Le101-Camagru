ad8f7dba5a0452b5880111f91afcb292032ac329
William Gerald Blondel <contact@williamblondel.fr>
2018-08-20 09:14:09 +0200

    Move CSS to the end of the page

--- app/Views/templates/shared/html.head.php
+++ app/Views/templates/shared/html.head.php
@@ -4,13 +4,3 @@
 <meta name="author" content="">
 
 <title><?= App::getInstance()->title;?></title>
-
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-<link rel="stylesheet" href="css/common.css" type="text/css" media="all">
-
-<?php if (isset($customcss)) : ?>
-    <?php foreach ($customcss as $filename) : ?>
-        <link rel="stylesheet" href="<?= $filename ?>" type="text/css" media="all">
-    <?php endforeach; ?>
-<?php endif ?>
