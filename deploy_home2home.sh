69fad480076cec23800a0b83c337f691d0847a40
William Blondel <contact@williamblondel.fr>
2018-11-05 03:04:37 +0100

    Update deploy scripts

--- deploy_home2home.sh
+++ deploy_home2home.sh
@@ -4,9 +4,9 @@ tar --exclude='.git' --exclude='.idea' --exclude='*.bak' --exclude='*.example' -
 echo "DONE"
 
 echo "Removing distant folder"
-ssh 2566335.xyz -p 15250 'rm -rf /usr/share/nginx/camagru.2566335.xyz/{*,.*}'
+ssh root@51.15.122.154 -i ~/.ssh/scaleway 'rm -rf /var/www/camagru.williamblondel.fr/{*,.*}'
 echo "DONE"
 
 echo "Extracting project to distant folder"
-cat /tmp/camagru.tar.gz | ssh 2566335.xyz -p 15250 "cd /usr/share/nginx/camagru.2566335.xyz; tar zxvf -"
+cat /tmp/camagru.tar.gz | ssh root@51.15.122.154 -i ~/.ssh/scaleway "cd /var/www/camagru.williamblondel.fr; tar zxvf -"
 echo "DONE"
