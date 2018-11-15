0388497c875ec237c35dd9c2e440a46b2b0db9c4
William Blondel <contact@williamblondel.fr>
2018-11-15 14:38:50 +0100

    Modify titles

--- app/Views/templates/gallery.php
+++ app/Views/templates/gallery.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="robots" content="noimageindex, noarchive">
     <title><?php if (isset($page_title)) { echo $page_title . ' &bull; '; }?><?= App::getInstance()->title;?></title>
 </head>
 
