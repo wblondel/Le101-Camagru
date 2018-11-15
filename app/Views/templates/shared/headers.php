7d4e16d32520712f39eb737fc098e0f7002d3180
William Blondel <contact@williamblondel.fr>
2018-11-15 17:34:53 +0100

    Reorganize templates

--- app/Views/templates/shared/headers.php
+++ app/Views/templates/shared/headers.php
@@ -2,22 +2,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="robots" content="noimageindex, noarchive">
 <title>
-    <?php
-    if (isset($image)) {
-        $title = $user->username . " sur " . App::getInstance()->title . ' : ' . '"' . $image->getLongDesc() . '"';
-        $description = $image->likesNb . "mentions J'aime, " . $image->commentsNb . " commentaires - " . $title;
-        echo $title;
-    } else {
-        if (isset($page_title)) {
-            echo $page_title . ' &bull; ';
-        }
-        echo App::getInstance()->title;
-    }
-    ?>
+    <?php if (isset($image)) : ?>
+        <?php
+            $title = $user->username . " sur " . App::getInstance()->title . ' : ' . '"' . $image->getLongDesc() . '"';
+            $description = $image->likesNb . "mentions J'aime, " . $image->commentsNb . " commentaires - " . $title;
+            echo $title;
+        ?>
+    <?php else : ?>
+        <?php if (isset($page_title)) : ?>
+            <?php echo $page_title . ' &bull; '; ?>
+        <?php endif ?>
+        <?php echo App::getInstance()->title; ?>
+    <?php endif ?>
 </title>
 
-<?php
-if (isset($image)) { ?>
+<?php if (isset($image)) : ?>
     <meta content="<?= $description ?>" name="description" />
     <meta property="og:site_name" content="<?= App::getInstance()->title;?>" />
     <meta property="og:title" content="<?= $title;?>" />
@@ -25,4 +24,4 @@ if (isset($image)) { ?>
     <meta property="og:description" content="<?= $description ?>" />
     <meta property="og:url" content="https://camagru.fr/images/<?= $image->id?>/"/>
     <meta name="medium" content="image"/>
-<?php}
+<?php endif ?>
