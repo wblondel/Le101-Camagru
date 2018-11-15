d1358ddff288b86b3a769c562f285c55237e6dc3
William Blondel <contact@williamblondel.fr>
2018-11-15 14:00:50 +0100

    Fix page titles

--- app/Views/templates/card-form.php
+++ app/Views/templates/card-form.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title><?php if (isset($page_title)) { echo $page_title; ?> | <?php}?><?= App::getInstance()->title;?></title>
+    <title><?php if (isset($page_title)) { echo $page_title . ' | '; }?><?= App::getInstance()->title;?></title>
 </head>
 
 <body>
