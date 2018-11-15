52afe54554b147be3ae42ebf4514dc4aa97365ce
William Blondel <contact@williamblondel.fr>
2018-11-15 18:51:51 +0100

    findWithUser

--- app/Table/ImageTable.php
+++ app/Table/ImageTable.php
@@ -32,7 +32,7 @@ class ImageTable extends Table
     public function findWithUser(int $id)
     {
         return $this->query("
-            SELECT images.*, users.username
+            SELECT images.*, users.username as `users.username`
             FROM images
             JOIN users ON images.users_id=users.id
             WHERE images.id = ?", [$id], true);
