245aefe125f676b183a0ccd91c8d841f95b0ef03
William Blondel <contact@williamblondel.fr>
2018-11-21 11:35:01 +0100

    fix intl

--- app/App.php
+++ app/App.php
@@ -76,27 +76,24 @@ class App
         // = locale =
         // ==========
 
+        $default_lang = "fr_FR.utf8";
 
-        if (is_null($this->lang_instance)) {
-            $default_lang = "fr_FR.utf8";
+        // here we define the global system locale given the found language
+        putenv("LANG=" . $default_lang);
 
-            // here we define the global system locale given the found language
-            putenv("LANG=" . $default_lang);
+        // this might be useful for date functions (LC_TIME) or money formatting (LC_MONETARY), for instance
+        setlocale(LC_ALL, $default_lang);
 
-            // this might be useful for date functions (LC_TIME) or money formatting (LC_MONETARY), for instance
-            setlocale(LC_ALL, $default_lang);
+        // this will make Gettext look for /locales/<lang>/LC_MESSAGES/main.mo
+        bindtextdomain('main', ROOT . '/locales');
 
-            // this will make Gettext look for /locales/<lang>/LC_MESSAGES/main.mo
-            bindtextdomain('main', ROOT . '/locales');
+        // indicates in what encoding the file should be read
+        bind_textdomain_codeset('main', 'UTF-8');
 
-            // indicates in what encoding the file should be read
-            bind_textdomain_codeset('main', 'UTF-8');
+        // here we indicate the default domain the gettext() calls will respond to
+        textdomain('main');
 
-            // here we indicate the default domain the gettext() calls will respond to
-            textdomain('main');
-
-            $this->lang_instance = $default_lang;
-        }
+        $this->lang_instance = $default_lang;
         return $this->lang_instance;
     }
 }
