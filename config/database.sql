936d7e91ac2c6fdd2e88141b8b875658236654bc
William Blondel <contact@williamblondel.fr>
2018-11-21 21:20:53 +0100

    remove innodb large prefix

--- config/database.sql
+++ config/database.sql
@@ -6,7 +6,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-SET @@global.innodb_large_prefix = 1;
 
 -- -----------------------------------------------------
 -- Table `users`
