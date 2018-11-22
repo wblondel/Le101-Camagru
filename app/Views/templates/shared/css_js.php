f30408925d11f5c40edb889d08b6fd3030e3b891
William Blondel <contact@williamblondel.fr>
2018-11-22 18:59:46 +0100

    update bootstrap css

--- app/Views/templates/shared/css_js.php
+++ app/Views/templates/shared/css_js.php
@@ -1,4 +1,4 @@
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href="/css/common.css" type="text/css" media="all">
 
 <?php if (isset($res)) : ?>
