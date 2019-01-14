ab613c1714b40d6c4c4c9050be0fffee6df0e42c
William Blondel <contact@williamblondel.fr>
2019-01-14 23:50:56 +0100

    Remove debug menu in footer

--- app/Views/templates/shared/footer.php
+++ app/Views/templates/shared/footer.php
@@ -1,10 +1,6 @@
 <footer class="text-muted">
     <div class="container">
         <p class="float-right">
-            <a href="admin"><?= _("ADMIN") ?></a>
-            <a href="accounts/register"><?= _("REGISTER") ?></a>
-            <a href="accounts/login"><?= _("LOGIN") ?></a>
-            <a href="accounts/logout"><?= _("LOGOUT") ?></a>
         </p>
         <p>&copy; LOLILOL Company</p>
         <p><?= _("Le 101 is a joke.") ?></p>
