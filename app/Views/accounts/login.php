d31aec565b0c134a70f3e9f55786b1ede8c0bff7
William Blondel <contact@williamblondel.fr>
2019-01-16 15:24:42 +0100

    Fix tab index

--- app/Views/accounts/login.php
+++ app/Views/accounts/login.php
@@ -3,7 +3,7 @@
 
     <div class="form-group">
         <label for="username"><?= _("Username") ?></label>
-        <input id="username" type="text" class="form-control" name="username" value="" required autofocus>
+        <input id="username" type="text" class="form-control" name="username" value="" tabindex="1" required autofocus>
     </div>
 
     <div class="form-group">
@@ -12,17 +12,17 @@
                 <?= _("Forgot Password?") ?>
             </a>
         </label>
-        <input id="password" type="password" class="form-control" name="password" required data-eye>
+        <input id="password" type="password" class="form-control" name="password" tabindex="2" required data-eye>
     </div>
 
     <div class="form-group">
         <label>
-            <input type="checkbox" name="remember"> <?= _("Remember Me") ?>
+            <input type="checkbox" name="remember" tabindex="3"> <?= _("Remember Me") ?>
         </label>
     </div>
 
     <div class="form-group no-margin">
-        <button type="submit" class="btn btn-primary btn-block">
+        <button type="submit" class="btn btn-primary btn-block" tabindex="4">
             <?= _("Login") ?>
         </button>
     </div>
