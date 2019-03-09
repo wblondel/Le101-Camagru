9dd5249718e981fc1e9954a9f5f245df566bf89d
William Gerald Blondel <contact@williamblondel.fr>
2019-03-09 19:31:12 +0100

    Removed useless links from navbar and added Post a picture link

--- app/Views/templates/shared/top_navbar.php
+++ app/Views/templates/shared/top_navbar.php
@@ -1,35 +1,34 @@
 <div id="app" class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-faded">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Camagru</a>
         <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">-->
         <!--<span class="navbar-toggler-icon"></span>-->
         <!--</button>-->
         <!--<div id="navbarNavDropdown" class="navbar-collapse collapse">-->
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="/"><?= _("Home") ?> <span class="sr-only"><?= _("(current)") ?></span></a>
+                <a class="nav-link" href="/"><?= _("Gallery") ?> <span class="sr-only"><?= _("(current)") ?></span></a>
             </li>
+            <?php if ($logged === true) : ?>
             <li class="nav-item">
-                <a class="nav-link" href="#"><?= _("A page...") ?></a>
+                <a class="nav-link" href="#"><?= _("Post a picture") ?></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"><?= _("Another page...") ?></a>
-            </li>
+            <?php endif; ?>
         </ul>
         <ul class="navbar-nav">
             <?php if ($logged === false) : ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="/accounts/login"><?= _("Login") ?></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/accounts/register"><?= _("Register") ?></a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/accounts/login"><?= _("Login") ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/accounts/register"><?= _("Register") ?></a>
+            </li>
             <?php else : ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="/accounts/logout"><?= _("Logout") ?></a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/accounts/logout"><?= _("Logout") ?></a>
+            </li>
             <?php endif; ?>
         </ul>
         <!--</div>-->
     </nav>
-</div>
+</div> 
