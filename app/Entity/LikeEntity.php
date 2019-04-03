7079bf8918bc666a12e286993f14549392cab26b
William Gerald Blondel <contact@williamblondel.fr>
2019-04-03 15:39:37 +0200

    Add like entity

---
+++ app/Entity/LikeEntity.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use Core\Entity\Entity;
+
+/**
+ * Class LikeEntity
+ *
+ * @package App\Entity
+ */
+class LikeEntity extends Entity
+{
+}
