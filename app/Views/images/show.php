e7ee44eabc1cb503d8c15ae9751c20bcf370a6e4
William Gerald Blondel <contact@williamblondel.fr>
2019-04-04 02:18:04 +0200

    comment box

--- app/Views/images/show.php
+++ app/Views/images/show.php
@@ -55,7 +55,7 @@
                                 </li>
                                 <?php endforeach; ?>
                             </ul>
-                            <form action="/c/add/<?= $singleImage->id ?>" method="post">
+                            <form action="/c/add/<?= $singleImage->id ?>" method="post" class="form-image-comment">
                                 <div class="form-row">
                                     <div class="col-10">
                                         <label class="sr-only" for="commentContent">Your comment</label>
