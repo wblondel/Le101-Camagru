bf9a6bd20e8336432124f0d3a5de36ed35288ddb
William Gerald Blondel <contact@williamblondel.fr>
2019-04-04 05:18:39 +0200

    now returns added comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -24,7 +24,7 @@ function processCommentForms()
                         commentsList = commentForm.parentElement.getElementsByClassName("commentList")[0];
                         commentToAppend = document.createElement('li');
                         /*TODO: Show username and add link to user profile*/
-                        commentToAppend.innerHTML = '<div class="commenterImage"><img src="https://placekitten.com/50/50"></div><div class="commentText"><p>' + results['comment']['username'] + ': ' + results['comment']['content'] + '</p><span class="date sub-text">' + results['comment']['createdDate'] + '</span></div>';
+                        commentToAppend.innerHTML = results['comment'];
                         commentsList.prepend(commentToAppend);
 
                     }
