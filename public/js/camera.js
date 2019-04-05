de9e83732db89c0e25e3d655c4e8ddd8a8bbab82
William Gerald Blondel <contact@williamblondel.fr>
2019-04-05 22:28:10 +0200

    upload image instead of webcam

--- public/js/camera.js
+++ public/js/camera.js
@@ -71,77 +71,81 @@ navigator.mediaDevices.getUserMedia({audio: false, video: true})
             video.play();
         };
 
-        button.addEventListener("click", function () {
-            var video = document.getElementById("video");
-
-            // change maxheight of history list
-            var historyList = document.getElementById("pictures-history");
-            historyList.style.maxHeight = video.clientHeight + "px";
-
-            // Get a screenshot of the video
-            if (video.tagName === "video") {
-                var screenshotCanvas = document.getElementById("screenshot-canvas");
-                screenshotCanvas.width = video.clientWidth;
-                screenshotCanvas.height = video.clientHeight;
-                var screenshotCanvasContext = screenshotCanvas.getContext("2d");
-                screenshotCanvasContext.drawImage(video, 0, 0, screenshotCanvas.width, screenshotCanvas.height);
-                var screenshotDataURI = screenshotCanvas.toDataURL('image/jpeg');
-            } else {
-                var screenshotDataURI = video.src;
-            }
-
-
-            // Get the effect
-            var effectDataURI = effectCanvas.toDataURL('image/jpeg');
-
-            // Get position of effect
-            var effectPosition = [effectCanvas.clientTop, effectCanvas.clientLeft];
-
-            // Send image to PHP
-            var formData = new FormData;
-            formData.append("screenshot", screenshotDataURI);
-            formData.append("effect", effectDataURI);
-            formData.append("position", effectPosition);
-            var xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4) {
-                    if (xhr.status != 200) {
-                        console.log("Can't comment.");
-                    } else {
-                        var results = JSON.parse(xhr.responseText);
-                        console.log(results);
-
-                        var imageToPut = results['imageFilename'];
-
-                        var picturesHistory = document.getElementById("pictures-history");
-                        divToAppend = document.createElement('li');
-                        divToAppend.innerHTML = '<img src="/uploads/pictures/' + imageToPut + '" class="img-fluid" alt="' + imageToPut + '">';
-                        picturesHistory.prepend(divToAppend);
-                        // Write the image to the history
-                        /*
-                        var canvas = document.getElementById("canvas");
-                        canvas.width = video.clientWidth;
-                        canvas.height = video.clientHeight;
-                        var c2d = canvas.getContext("2d");
-                        c2d.drawImage(video, 0, 0, canvas.width, canvas.height);
-                        var image = document.getElementById("photo");
-                        image.src = canvas.toDataURL("image/png");
-                        */
-
-                    }
-                }
-            };
-
-            xhr.open('POST', '/i/new', true);
-            xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
-            xhr.send(formData);
-        })
+        button.addEventListener("click", takePhoto);
     })
     .catch(function (err) {
         console.log(err.name + ": " + err.message);
     });
 
+
+function takePhoto()
+{
+    var video = document.getElementById("video");
+    var button = document.getElementById('startbutton');
+
+    // change maxheight of history list
+    var historyList = document.getElementById("pictures-history");
+    historyList.style.maxHeight = video.clientHeight + "px";
+
+    // Get a screenshot of the video
+    if (video.tagName === "video") {
+        var screenshotCanvas = document.getElementById("screenshot-canvas");
+        screenshotCanvas.width = video.clientWidth;
+        screenshotCanvas.height = video.clientHeight;
+        var screenshotCanvasContext = screenshotCanvas.getContext("2d");
+        screenshotCanvasContext.drawImage(video, 0, 0, screenshotCanvas.width, screenshotCanvas.height);
+        var screenshotDataURI = screenshotCanvas.toDataURL('image/jpeg');
+    } else {
+        var screenshotDataURI = video.src;
+    }
+
+
+    // Get the effect
+    var effectDataURI = effectCanvas.toDataURL('image/jpeg');
+
+    // Get position of effect
+    var effectPosition = [effectCanvas.clientTop, effectCanvas.clientLeft];
+
+    // Send image to PHP
+    var formData = new FormData;
+    formData.append("screenshot", screenshotDataURI);
+    formData.append("effect", effectDataURI);
+    formData.append("position", effectPosition);
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status != 200) {
+                console.log("Can't comment.");
+            } else {
+                var results = JSON.parse(xhr.responseText);
+                console.log(results);
+
+                var imageToPut = results['imageFilename'];
+
+                var picturesHistory = document.getElementById("pictures-history");
+                divToAppend = document.createElement('li');
+                divToAppend.innerHTML = '<img src="/uploads/pictures/' + imageToPut + '" class="img-fluid" alt="' + imageToPut + '">';
+                picturesHistory.prepend(divToAppend);
+                // Write the image to the history
+                /*
+                var canvas = document.getElementById("canvas");
+                canvas.width = video.clientWidth;
+                canvas.height = video.clientHeight;
+                var c2d = canvas.getContext("2d");
+                c2d.drawImage(video, 0, 0, canvas.width, canvas.height);
+                var image = document.getElementById("photo");
+                image.src = canvas.toDataURL("image/png");
+                */
+
+            }
+        }
+    };
+
+    xhr.open('POST', '/i/new', true);
+    xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+    xhr.send(formData);
+}
 
 // ----------------------------------------------
 
